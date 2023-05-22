b629acb6ea616ad5155479e92f451c61dd827d89
Sergei <zagers72@bk.ru>
2023-05-22 12:32:34 +0500

    Задание 4

---
+++ D4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+int num1 = 22, num2 = 3, num3 = 9;
+ int max;
+ if (num1 >= num2)
+ {
+    if (num3 >= num1)
+    {
+       Console.WriteLine(max = num3);
+    }
+    else
+    {
+      Console.WriteLine(max = num1);
+    }
+ }
+ else
+ {
+    if (num3 >= num2)
+    {
+       Console.WriteLine(max = num3);
+    }
+    else
+    {
+       Console.WriteLine(max = num2);
+    }
+ }

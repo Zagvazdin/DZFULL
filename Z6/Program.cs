ac42e13862ba76020c347c6e1db0cee7b60bea18
Sergei <zagers72@bk.ru>
2023-05-22 12:46:14 +0500

    Задача 6

---
+++ Z6/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Ведите  значение :");
+int number1 = int.Parse(Console.ReadLine());
+int resultat = number1 % 2;
+if (resultat == 0)
+{
+Console.WriteLine("ДА " + number1);
+}
+else 
+{
+    Console.WriteLine("НЕТ " + number1);
+}
